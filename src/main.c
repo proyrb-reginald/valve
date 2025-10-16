ff7fd6054043e8fc160e4040bed5d96561c902f6
proyrb <proyrb@yeah.net>
2025-10-16 16:34:52 +0800

    初步构建项目，并裸机状态下正常进行周期的串口输出。

---
+++ src/main.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include "i2c.h"
+#include "usart.h"
+#include "gpio.h"
+
+void SystemClock_Config(void);
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_USART1_UART_Init();
+    while (1) {
+        HAL_Delay(500);
+        HAL_UART_Transmit(&huart1, (uint8_t *)"Hello World!\r\n", 14, 1000);
+    }
+}
+
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitStruct.OscillatorType     = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState           = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue     = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState           = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState       = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource      = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL         = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
+    HAL_RCC_EnableCSS();
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM1) {
+        HAL_IncTick();
+    }
+}
+
+void Error_Handler(void)
+{
+    __disable_irq();
+    while (1) {
+    }
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
